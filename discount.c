7dbe266c6b34f024a44e43af7049b8e23f7c7eea
stephennyagah <123065585+stephennyagah@users.noreply.github.com>
2023-02-03 16:54:14 +0300

    Add files via upload

---
+++ discount.c
@@ -0,0 +1,12 @@
+// program to find if one qualify a discount
+#include <stdio.h>
+int main()
+ {
+   int amount; 
+    printf("Enter the amount");
+scanf("%d",&amount);
+if("amount>5000");
+{printf("you qualify discount");}
+{printf("you do not qualify");}
+    return 0;
+}
