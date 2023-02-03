ab4d69de0d83ea56907a14ea3555640a38a8cd7f
stephennyagah <123065585+stephennyagah@users.noreply.github.com>
2023-02-03 16:21:08 +0300

    Add files via upload

---
+++ vote.c
@@ -0,0 +1,13 @@
+// program to find if one eligible to vote
+#include <stdio.h>
+int main()
+ {
+int age,citizenship;
+    printf("\nEnter the  age and citizenship");
+scanf("%d,%d",&age,&citizenship);
+if(age>18 && citizenship);
+{printf("eligible to vote");}
+{
+printf("not eligible to vote");}
+    return 0;
+}
