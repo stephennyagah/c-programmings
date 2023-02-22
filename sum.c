c3efc579c1759cc50462794dc5f320274b046721
stephennyagah <123065585+stephennyagah@users.noreply.github.com>
2023-02-22 15:02:59 +0300

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,13 @@
+// program to find the sum of first 10 natural numbers
+#include <stdio.h>
+int main() {
+    int n,sum ;
+
+    printf("enter values of n: ");
+    scanf("%d", &n);
+
+    printf(n>=1&&n<=10);
+    {(sum=1+2+3+4+5+6+7+8+9+10);}
+    printf("sum=55");
+    return 0;
+}
