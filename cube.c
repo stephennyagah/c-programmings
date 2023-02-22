d127d0d0187ccb8434ac9a56008340599348b14d
stephennyagah <123065585+stephennyagah@users.noreply.github.com>
2023-02-22 16:15:18 +0300

    Add files via upload

---
+++ cube.c
@@ -0,0 +1,11 @@
+//program to find cube of an integer
+#include <stdio.h>
+int main() 
+{
+int a,cubic;   
+    printf("enter the values of a\n:");
+scanf("%d",&a);
+cubic=a*a*a;
+printf("\n,cubic=%d");
+    return 0;
+}
