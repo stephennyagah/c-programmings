3b74c6f1fcd704d5fc98667fe595db9cfeee2785
stephennyagah <123065585+stephennyagah@users.noreply.github.com>
2023-02-22 13:07:35 +0300

    Add files via upload

---
+++ reverse.c
@@ -0,0 +1,12 @@
+//program to display first 50 natural numbers in reverse form 
+#include <stdio.h>
+int main()
+{
+    int counter,n;
+    printf("enter the positive numbers \n");
+scanf ("%d",&n);
+printf("print numbers from 50 to 1 \n");
+for(counter=50;counter>0;counter)
+{printf("50\n",counter);}
+    return 0;
+}
