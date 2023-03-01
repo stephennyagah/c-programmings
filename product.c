841777bf43113ce3d467c6ba0dc04948e9da1ca6
stephennyagah <123065585+stephennyagah@users.noreply.github.com>
2023-03-01 11:44:45 +0300

    Add files via upload

---
+++ product.c
@@ -0,0 +1,12 @@
+//program to find product
+#include <stdio.h>
+int main()
+{
+    int a, b, product;
+    printf("Enter two numbers\n: ");
+    scanf("%d %d", &a, &b);  
+    product = a * b;
+    printf("Product =%d", product);
+
+    return 0;
+}
