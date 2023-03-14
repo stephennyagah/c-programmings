cfcd17935008b8b128c5d9d9135c7af01b261a35
stephennyagah <123065585+stephennyagah@users.noreply.github.com>
2023-03-14 09:13:38 +0300

    Add files via upload

---
+++ 2 d array.c
@@ -0,0 +1,19 @@
+//program for 2 d array
+#include<stdio.h>
+int main()
+{
+    int i, j,marks [2][2] = 
+    {{
+     {1, 2}, {4, 6} },
+        { {9,10}, {7,5} },
+        { {0,2}, {5,5}
+        }};
+    for(i=0; i<2; i++)
+    {
+        for(j=0; j<2; j++)
+        {
+                printf("%d",marks,i,j);
+                
+            }}
+    return 0;
+}
